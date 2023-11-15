cf3d6d975581aec3711914075a4f852ba9554002
AugustoM94 <email@github.com>
2023-11-15 16:32:36 +0100

    added components HeaderApp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-
-  </div>
+    <header class="d-flex justify-content-between align-items-center bg-black ">
+        <HeaderApp/>
+    </header>
 </template>
 
 <script>
+import { store } from './assets/data/store';
+import axios from 'axios';
+import HeaderApp from './components/HeaderApp.vue';
   export default {
-    
+    name: 'App',
+  components: {
+    HeaderApp
+  },
+  data() {
+    return {
+      store,
+
+    };
+  },
   }
 </script>
 
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,21 @@
+<template>
+ <div >
+    <h1>BOOLFLIX</h1>
+ </div>
+ <div class="align-items-center">
+    <input type="text">
+    <button></button>
+ </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+h1{
+    color: red;
+}
+</style>
