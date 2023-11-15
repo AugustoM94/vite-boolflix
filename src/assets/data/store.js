5bd001ebe7524a0f2226e8a5e1cb0497163cdc06
AugustoM94 <email@github.com>
2023-11-15 16:37:11 +0100

    added store.js

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -1,5 +1,18 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+apiUrl: "https://api.themoviedb.org/3/",
+endPoint: {
+    movies: "search/movie",
+    series: "search/tv",
+},
+params: {
+    api_key:"8e54bd6b4cea1826ba363b44600af80c ",
+    query: "a",
+},
 
+    error:"",
+    loading:true,
+    movieList:[],
+    seriesList:[],
 });
